e1fa75fc46f26a7bcb7ae8ed4b7b9b7f8907c3bc
cssupratik <cs.supratik@zoho.com>
2020-04-19 01:07:48 -0400

    starting database connection properties

---
+++ backdrop1/src/main/java/backdropv1/model/NewUser.java
@@ -0,0 +1,47 @@
+package backdropv1.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "userinfo")
+public class NewUser {
+	private String firstName;
+	private String lastName;
+	private String userName;
+	private String passWord;
+	private String emailid;
+	
+	public String getEmailid() {
+		return emailid;
+	}
+	public void setEmailid(String emailid) {
+		this.emailid = emailid;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassWord() {
+		return passWord;
+	}
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+	
+	
+}
